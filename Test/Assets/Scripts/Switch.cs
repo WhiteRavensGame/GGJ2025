ed673d0c9e3c72ac491aa364b9d5fc3886fb7655
WhiteRavensGame <cuecharles@gmail.com>
2025-01-26 00:43:12 -0800

    Level 7 Completed

---
+++ Test/Assets/Scripts/Switch.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{ 
+    public GameObject objectToHide;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!objectToHide.activeInHierarchy)
+            return;
+
+        if(other.tag == "PhysicObstacles" || other.tag == "Player")
+        {
+            objectToHide.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "PhysicObstacles" || other.tag == "Player")
+        {
+            objectToHide.SetActive(true);
+        }
+    }
+}
